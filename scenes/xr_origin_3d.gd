0c4dbae9b21d9304f5acc66a6b19aded832b7fe5
antlilja <liljaanton2001@gmail.com>
2024-11-19 12:01:49 +0100

    Add basic snowball creation and physics

--- scenes/xr_origin_3d.gd
+++ scenes/xr_origin_3d.gd
@@ -1,6 +1,7 @@
 extends XROrigin3D
 
-
+var snowball_scene = preload("res://scenes/snowball.tscn")
+var current_snowball: RigidBody3D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,12 +14,27 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+func create_snowball_on_hand(hand_node: XRNode3D) -> void:
+	var snowball: RigidBody3D = snowball_scene.instantiate()
+	current_snowball = snowball
+	hand_node.add_child(snowball)
+	pass
+	
 func _on_right_hand_button_pressed(name: String) -> void:
-	print(name)
+	match name:
+			"trigger_click": create_snowball_on_hand($RightHand)
 	pass # Replace with function body.
 
 
 func _on_right_hand_input_float_changed(name: String, value: float) -> void:
 	print(name + " value: " + str(value))
+	pass # Replace with function body.
+
+
+func _on_right_hand_button_released(name: String) -> void:
+	print(name)
+	match name:
+			"trigger_click": 
+				if (current_snowball != null):
+					current_snowball.reparent($".")
 	pass # Replace with function body.
