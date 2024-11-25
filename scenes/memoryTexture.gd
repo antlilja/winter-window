929e37b70e62661f877bdef4c72c6863c11a2c86
Sebbe <sebastian_hellman@live.se>
2024-11-25 19:18:38 +0100

    Memory texture no work

--- scenes/memoryTexture.gd
+++ scenes/memoryTexture.gd
@@ -2,7 +2,7 @@
 extends Node3D
 
 var imageTexture
-@onready var viewportTexture = $ground.get_instance_shader_parameter("viewportTexture")
+@onready var viewportTexture = $ground.material_override.get_shader_parameter("viewportTexture")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,13 +11,13 @@ func _ready() -> void:
 	for x in range(512):
 		for y in range(512):
 			imageTexture.set_pixel(x, y, 0)
-	$ground.set_instance_shader_parameter("memoryTexture", imageTexture)
+	$ground.material_override.set_shader_parameter("memoryTexture", imageTexture)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(viewportTexture.get_size())
-	#for x in range(512):
-		#for y in range(512):
-			#if data.get_pixel(x,y) != Color(0, 0, 0, 1):
-			#	imageTexture.set_pixel(x, y, data.get_pixel(x,y))
-	$ground.set_instance_shader_parameter("memoryTexture", imageTexture)
+	var data = viewportTexture.get_image()
+	for x in range(512):
+		for y in range(512):
+			if data.get_pixel(x,y) != Color(0, 0, 0, 1):
+				imageTexture.set_pixel(x, y, data.get_pixel(x,y))
+	$ground.material_override.set_shader_parameter("memoryTexture", imageTexture)
