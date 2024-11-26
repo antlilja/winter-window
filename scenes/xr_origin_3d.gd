3428be72ab23de38d1148851f0ec22c9cd86f216
Isak Larsson <isak.valter.larsson@gmail.com>
2024-11-26 14:22:16 +0100

    Remove hand snowball creation and change grab input Co-authored-by: Jessica Gorwat <jessicagorwat@gmail.com>

--- scenes/xr_origin_3d.gd
+++ scenes/xr_origin_3d.gd
@@ -14,33 +14,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func create_snowball_on_hand(hand_node: XRNode3D) -> void:
-	var snowball: RigidBody3D = snowball_scene.instantiate()
-	current_snowball = snowball
-	current_snowball
-	hand_node.add_child(current_snowball)
-	current_snowball.freeze = true
-	pass
-	
-func _on_right_hand_button_pressed(name: String) -> void:
-	match name:
-			"trigger_click": create_snowball_on_hand($RightHand)
-	pass # Replace with function body.
-
-
-func _on_right_hand_input_float_changed(name: String, value: float) -> void:
-	print(name + " value: " + str(value))
-	pass # Replace with function body.
-
-
-func _on_right_hand_button_released(name: String) -> void:
-	print(name)
-	match name:
-			"trigger_click": 
-				if (current_snowball != null):
-					current_snowball.freeze = false
-					current_snowball.reparent($".")
-					current_snowball.linear_velocity = right_hand.get_velocity()
-					current_snowball = null
-	pass # Replace with function body.
