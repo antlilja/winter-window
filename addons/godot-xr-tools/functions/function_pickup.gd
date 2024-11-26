479fff872d2d79989d30ccb795c36af27e70c5c7
Isak Larsson <isak.valter.larsson@gmail.com>
2024-11-26 14:21:22 +0100

    Add snowball creation on ground Co-authored-by: Jessica Gorwat <jessicagorwat@gmail.com>

--- addons/godot-xr-tools/functions/function_pickup.gd
+++ addons/godot-xr-tools/functions/function_pickup.gd
@@ -3,6 +3,7 @@
 class_name XRToolsFunctionPickup
 extends Node3D
 
+var snowball_scene = preload("res://scenes/snowball_pickable.tscn")
 
 ## XR Tools Function Pickup Script
 ##
@@ -424,6 +425,10 @@ func _on_grip_pressed() -> void:
 	if is_instance_valid(picked_up_object) and !picked_up_object.press_to_hold:
 		drop_object()
 	elif is_instance_valid(closest_object):
+if closest_object.is_in_group("ground"):
+			var snowball = snowball_scene.instantiate()
+			get_tree().root.add_child(snowball)
+			closest_object = snowball
 		_pick_up_object(closest_object)
 
 
