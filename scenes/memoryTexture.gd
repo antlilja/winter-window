675d66a8387d6d533b8ec0191b8372abdc73b7c4
Sebbe <sebastian_hellman@live.se>
2024-12-05 12:26:19 +0100

    footsteps

--- scenes/memoryTexture.gd
+++ scenes/memoryTexture.gd
@@ -6,8 +6,9 @@ var regen = 2.0
 var time = 0.0
 @onready var viewportTexture = $ground.material_override.get_shader_parameter("viewportTexture")
 var memTexture
-
-
+var playSound = true
+var timeToNextSound = 0
+@onready var footsteps = $AudioStreamPlayer3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var mat = $ground.get_active_material(0)
@@ -24,6 +25,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time += delta
+	viewportTexture = $ground.material_override.get_shader_parameter("viewportTexture")
+	memTexture = $ground.material_override.get_shader_parameter("memoryTexture")
 	var data = viewportTexture.get_image()
 	var memData = memTexture.get_image()
 	for x in range(512):
@@ -37,6 +40,10 @@ func _process(delta: float) -> void:
 			# Deforms new snow
 			if pixel != Color(0, 0, 0, 1):
 				image.set_pixel(x, y, pixel)
+				if playSound and memPixel != pixel:
+					footsteps.play()
+					timeToNextSound = 1.0
+					playSound = false
 				
 	#		var newPixel = memPixel
 			#Check previous snow and check if it should regenerate
@@ -57,3 +64,6 @@ func _process(delta: float) -> void:
 	var texture = ImageTexture.create_from_image(image)
 	$ground.material_override.set_shader_parameter("memoryTexture", texture)
 	#print($ground.material_override.get_shader_parameter("memoryTexture"))	
+	timeToNextSound -= delta
+	if timeToNextSound <= 0:
+		playSound = true
