0ead7c1737520d6fac00ff1e97028b94c3954e45
antlilja <liljaanton2001@gmail.com>
2024-12-13 18:39:46 +0100

    Fix local size in convert_depth.glsl

--- shaders/convert_depth.glsl
+++ shaders/convert_depth.glsl
@@ -2,7 +2,7 @@
 #version 450
 
 // Invocations in the (x, y, z) dimension
-layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
 layout(set = 0, binding = 0, std430) restrict buffer DepthBufferIn {
 	uint data[];
